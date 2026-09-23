62101012c7d8fae7a176edc434feae0f4d9ef1a3
IAmAderrr <ader.ph123@gmail.com>
2026-09-23 12:44:19 +0000

    Delete hack-f917f57e-404-brain-not-found directory

--- hack-f917f57e-404-brain-not-found/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/BAITC-Hacks/hack-f917f57e-404-brain-not-found
-
-go 1.27.0
--- hack-f917f57e-404-brain-not-found/internal/careerquest/doc.go
+++
@@ -1,4 +0,0 @@
-// Package careerquest implements employee development workflows, account
-// management, dataset imports, and local persistence. The command in
-// cmd/career-quest provides configuration and process lifecycle handling.
-package careerquest
